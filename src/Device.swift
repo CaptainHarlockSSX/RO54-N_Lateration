296f9d95f571cdf3b4109167dc7de1f17a2b02c2
CaptainHarlockSSX <thedarklink12mc@gmail.com>
2023-03-04 20:04:17 +0100

    Implement Device protocol

--- src/Device.swift
+++ src/Device.swift
@@ -5,5 +5,16 @@
 //  Created by Nathan Rassié on 03/03/2023.
 //
 
-import Foundation
+/// Device protocol
+/// Define a device by its position in a three-dimensional world
+/// and by its ip adress.
+protocol Device: Equatable, Hashable {
+	// Device position in a three-dimensional world
+	var x: Float {get set}
+	var y: Float {get set}
+	var z: Float {get set}
+	
+	// IP adress of the device
+	var ipAdress: String {get set}
+}
 
