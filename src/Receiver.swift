0304728126bc49d9edf9fdfda49bb415a7b3b575
CaptainHarlockSSX <thedarklink12mc@gmail.com>
2023-03-06 14:03:00 +0100

    Add documentation to Receiver structure.

--- src/Receiver.swift
+++ src/Receiver.swift
@@ -7,10 +7,13 @@
 
 import Foundation
 
+/// Receiver structure, following Device protocol.
+/// Define an receiver by its position in a three-dimensional world,
+/// its distance to the emitter and by its ip adress.
 struct Receiver: Device {
     var x: Float?
     var y: Float?
     var z: Float?
-    var distance: Float
+    var distance: Float // Distance to the Emitter, given by the exercise
     var ipAdress: String
 }
