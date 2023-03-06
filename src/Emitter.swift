9357778a0b05a86ade14435f25dcf63f6edeb9ae
CaptainHarlockSSX <thedarklink12mc@gmail.com>
2023-03-06 13:45:35 +0100

    Add documentation for Emitter structure

--- src/Emitter.swift
+++ src/Emitter.swift
@@ -7,6 +7,11 @@
 
 import Foundation
 
+/// Emitter structure, following Device protocol.
+/// Define an emitter by its position in a three-dimensional world
+/// and by its ip adress. Position may be unknown as we are trying
+/// to solve it through N-Lateration algorithm, so the coordinates
+/// are optionals.
 struct Emitter: Device {
     var x: Float? = nil
     var y: Float? = nil
