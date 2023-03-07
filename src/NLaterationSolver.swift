95519f9e772affd492e0759f5647666d142fd340
Nicolas DERAISIN <nicolas.deraisin@utbm.fr>
2023-03-07 01:47:31 +0100

    add solveEmmiterPosition

--- src/NLaterationSolver.swift
+++ src/NLaterationSolver.swift
@@ -36,9 +36,48 @@ struct NLaterationSolver {
 	/// with x, y, z coordinates if the operation succeed, nil otherwise.
 	func solveEmitterPosition() -> [Float]? {
 		
-		if(getReceiverCount() < 4) {return nil}
+		if(getReceiverCount() < 4) else {return nil}
 		
 		return [0.0, 0.0, 0.0]
     }
+
+    func solveEmitterPosition(receivers: [String: Receiver], emitter: Emitter) -> [Float]? {
+    guard receivers.count >= 4 else { return nil }
+    
+    var a = [Float]()
+    var b = [Float]()
+    
+    // Iterate through all receiver pairs
+    for (ip1, r1) in receivers {
+        for (ip2, r2) in receivers {
+            guard ip1 != ip2 else { continue }
+            
+            let dist = r1.distance - r2.distance
+            let x = (r2.x! - r1.x!) / dist
+            let y = (r2.y! - r1.y!) / dist
+            let z = (r2.z! - r1.z!) / dist
+            let d = sqrt(pow(r1.distance, 2) - pow(r2.distance, 2) - pow(r1.x!, 2) + pow(r2.x!, 2) - pow(r1.y!, 2) + pow(r2.y!, 2) - pow(r1.z!, 2) + pow(r2.z!, 2))
+            
+            // Add to matrix A and vector B
+            a += [x, y, z]
+            b.append(0.5 * d)
+        }
+    }
+    
+    // Convert A and B to matrices
+    let aMat = Matrix(rows: receivers.count * (receivers.count - 1), columns: 3, elements: a)
+    let bMat = Matrix(rows: receivers.count * (receivers.count - 1), columns: 1, elements: b)
+    
+    // Calculate pseudo-inverse of A
+    guard let aInv = aMat.pseudoInverse() else { return nil }
+    
+    // Calculate position of emitter
+    let x = (aInv * bMat)[0, 0]
+    let y = (aInv * bMat)[1, 0]
+    let z = (aInv * bMat)[2, 0]
+    
+    return [x, y, z]
+}
+
 	
 }
