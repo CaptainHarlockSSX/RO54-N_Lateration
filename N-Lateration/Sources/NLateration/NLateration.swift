8c02459b3d6f4cb6bcb3b64dc843d4c654858e19
CaptainHarlockSSX <thedarklink12mc@gmail.com>
2023-03-13 11:36:28 +0100

    Refactor N-Lateration Solver function to process Emitter position
    - Comment previous work on N-Lateration solver function
    - Add new implementation
    - Edit @main function to provide tests

--- N-Lateration/Sources/NLateration/NLateration.swift
+++ N-Lateration/Sources/NLateration/NLateration.swift
@@ -2,22 +2,26 @@
 public struct NLateration {
     public static func main() {
 	
-		// Test to try if the package is working
-		let receiver1 = Receiver(x: 2.0, y: 3.0, z: 1.0, distance: 2.4, ipAdress: "192.168.0.0")
-		let receiver2 = Receiver(x: 4.0, y: 5.0, z: 6.0, distance: 3.4, ipAdress: "192.168.0.1")
-		let receiver3 = Receiver(x: 7.0, y: 1.0, z: 6.0, distance: 5.2, ipAdress: "192.168.0.6")
-		let receiver4 = Receiver(x: 3.0, y: 4.0, z: 1.0, distance: 1.4, ipAdress: "192.168.0.2")
-		let emitter1 = Emitter(ipAdress: "25.0.0.0")
+		// Create some receivers and the emitter to geolocalize
+		let receiver1 = Receiver(x: 0.5, y: 0.5, z: 0.5, distance: 3, ipAdress: "192.168.0.0")
+		let receiver2 = Receiver(x: 4.0, y: 0.0, z: 0.0, distance: 2, ipAdress: "192.168.0.1")
+		let receiver3 = Receiver(x: 4.0, y: 5.0, z: 5.0, distance: 4.2, ipAdress: "192.168.0.6")
+		let receiver4 = Receiver(x: 3.0, y: 3.0, z: 3.0, distance: 2.5, ipAdress: "192.168.0.2")
+		let emitter1 = Emitter(name: "iPhone 14", ipAdress: "25.0.0.0")
 		
+		// Load the n-lateration solver
 		var nLateration = NLaterationSolver(emitter: emitter1)
 		nLateration.add(receiver:receiver1,withIpAdress:receiver1.ipAdress)
 		nLateration.add(receiver:receiver2,withIpAdress:receiver2.ipAdress)
 		nLateration.add(receiver:receiver3,withIpAdress:receiver3.ipAdress)
 		nLateration.add(receiver:receiver4,withIpAdress:receiver4.ipAdress)
 		
-		try! nLateration.solveEmitterPosition()
-		//let emitterPosition = try! nLateration.solveEmitterPosition()
-		
-		//print("The emitter coordinates are \(String(describing: emitterPosition)), solved with \(nLateration.getReceiverCount()) receivers.")
+		// Solve emitter's position
+		let emitterPosition = nLateration.solveEmitterPosition()
+		if (emitterPosition != nil) {
+			print("✅ The emitter coordinates are \(emitterPosition!), solved with \(nLateration.getReceiverCount()) receivers.")
+		} else {
+			print("❌ Failed to solve emitter position with \(nLateration.getReceiverCount()) receivers. Ensure you have at least 4 Receivers with their position and distance loaded in the N-Lateration solver.")
+		}
     }
 }
--- N-Lateration/Sources/NLateration/NLaterationSolver.swift
+++ N-Lateration/Sources/NLateration/NLaterationSolver.swift
@@ -6,13 +6,16 @@
 //
 
 import Foundation
-import simd
+//import simd
 
+/// N-Lateration Solver Structure
+/// Geolocalize a Device by providing one Emitter and at least
+/// four Receivers with filled position and distance.
 struct NLaterationSolver {
 	private var receivers: [String: Receiver] = [:] // Dictionnary of Receivers [ipAdress: Receiver]
     private var emitter: Emitter
-	private var minAreaPoint: [Float] = []
-	private var maxAreaPoint: [Float] = []
+	private var minAreaPoint = [Float](repeating: 0.0, count: 3)
+	private var maxAreaPoint = [Float](repeating: 0.0, count: 3)
 	
 	init(receivers: [String : Receiver]? = [:], emitter: Emitter) {
 		self.receivers = receivers!
@@ -42,58 +45,88 @@ struct NLaterationSolver {
 	/// Try to solve position of the emitter by minimizing
 	/// the sum of distances to all Receivers. Return an optional
 	/// with x, y, z coordinates if the operation succeed, nil otherwise.
-	mutating func solveEmitterPosition() throws -> [Float]? {
+	mutating func solveEmitterPosition(withStep step: Float = 0.1) -> [Float]? {
 		
 		// Ensure there is four or more receivers in the dictionnary to be able to solve the emitter position
-		guard getReceiverCount() >= 4 else {fatalError("Not enough Receivers (\(getReceiverCount()) < 4) to find Emitter position.") }
+		guard getReceiverCount() >= 4 else { return nil }
 		
 		// Reduce area of research
-		//computeResearchArea()
+		computeResearchArea()
 		
-		var a = [Float]()
-		var b = [Float]()
+		// Process each sum of distances in the searching area to find the lowest one
+		// Initializing default value of min distance by using a random receiver's distance
+		var minDistance: Float = 0.0
+		for (_, receiver) in receivers { minDistance += receiver.distance } // Initialize defaut minimum distance value
+		var emitterPosition: [Float]?
+		
+		for x in stride(from: minAreaPoint[0], to: maxAreaPoint[0], by: step) {
+			for y in stride(from: minAreaPoint[1], to: maxAreaPoint[1], by: step) {
+				for z in stride(from: minAreaPoint[2], to: maxAreaPoint[2], by: step) {
+					
+					// Process and sum distances to all Receivers
+					var distance: Float = 0.0
+					
+					for (_, receiver) in receivers {
+						distance += abs(sqrtf(pow(x - receiver.x!,2) + pow(y - receiver.y!,2) + pow(z - receiver.z!,2)) - receiver.distance)
+					}
+					
+					// Keep the lowest distance
+					if distance < minDistance {
+						minDistance = distance
+						emitterPosition = [x, y, z]
+					}
+					
+				}
+			}
+		}
+		
+		return emitterPosition
+//		var a = [Float]()
+//		var b = [Float]()
 		
 //		var minDistance: Float
 		
 		// Iterate through all receiver pairs
-		for (ip1, r1) in receivers {
-			for (ip2, r2) in receivers {
-				guard ip1 != ip2 else { continue } // Skip iteration if the receivers are the same one
-				
-				let dist = r1.distance - r2.distance
-				let x = (r2.x! - r1.x!) / dist
-				let y = (r2.y! - r1.y!) / dist
-				let z = (r2.z! - r1.z!) / dist
-				let d = sqrt(pow(r1.distance, 2) - pow(r2.distance, 2) - pow(r1.x!, 2) + pow(r2.x!, 2) - pow(r1.y!, 2) + pow(r2.y!, 2) - pow(r1.z!, 2) + pow(r2.z!, 2))
-				
-				// Add to matrix A and vector B
-				a.append(SIMD3<Float>(x, y, z))
-				b.append(0.5 * d)
-			}
-		}
+//		for (ip1, r1) in receivers {
+//			for (ip2, r2) in receivers {
+//				guard ip1 != ip2 else { continue } // Skip iteration if the receivers are the same one
+//
+//				let dist = r1.distance - r2.distance
+//				let x = (r2.x! - r1.x!) / dist
+//				let y = (r2.y! - r1.y!) / dist
+//				let z = (r2.z! - r1.z!) / dist
+//				let d = sqrt(pow(r1.distance, 2) - pow(r2.distance, 2) - pow(r1.x!, 2) + pow(r2.x!, 2) - pow(r1.y!, 2) + pow(r2.y!, 2) - pow(r1.z!, 2) + pow(r2.z!, 2))
+//
+//				// Add to matrix A and vector B
+//				a.append(SIMD3<Float>(x, y, z))
+//				b.append(0.5 * d)
+//			}
+//		}
 		
 		
 		// Convert A and B to matrices
-		let aMat = float3x3(a[0], a[1], a[2])
-		let bVec = SIMD3<Float>(b[0], b[1], b[2])
-
-		for i in 3..<a.count {
-			aMat += float3x3(a[i-2], a[i-1], a[i])
-			bVec += SIMD3<Float>(b[i], b[i-1], b[i-2])
-		}
+//		var aMat = float3x3(a[0], a[1], a[2])
+//		var bVec = SIMD3<Float>(b[0], b[1], b[2])
+//
+//		for i in 3..<a.count {
+//			aMat += float3x3(a[i-2], a[i-1], a[i])
+//			bVec += SIMD3<Float>(b[i], b[i-1], b[i-2])
+//		}
 		
 
 
 		// Calculate pseudo-inverse of A
-		guard let aInv = aMat.pseudoInverse() else { return nil }
+//		guard let aInv = aMat.pseudoInverse() else { return nil }
 		
 		// Calculate position of emitter
 //		let x = (aInv * bMat)[0, 0]
 //		let y = (aInv * bMat)[1, 0]
 //		let z = (aInv * bMat)[2, 0]
-		let pos = aInv * bVec
+//		let pos = aInv * bVec
+		
+		
 //		return [0,0,0]
-		return [x, y, z]
+//		return [x, y, z]
 	}
 	
 	/// Reduce the area where the Emitter could be by
