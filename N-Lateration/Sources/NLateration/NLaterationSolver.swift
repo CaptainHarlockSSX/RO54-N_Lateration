8d81d1ef56ce4470a8bc0a1795d6d9c451c84ae6
vboxuser <vboxuser@UbuntuFull.myguest.virtualbox.org>
2023-03-17 09:16:51 +0100

    delete useless code

--- N-Lateration/Sources/NLateration/NLaterationSolver.swift
+++ N-Lateration/Sources/NLateration/NLaterationSolver.swift
@@ -81,52 +81,6 @@ struct NLaterationSolver {
 		}
 		
 		return emitterPosition
-//		var a = [Float]()
-//		var b = [Float]()
-		
-//		var minDistance: Float
-		
-		// Iterate through all receiver pairs
-//		for (ip1, r1) in receivers {
-//			for (ip2, r2) in receivers {
-//				guard ip1 != ip2 else { continue } // Skip iteration if the receivers are the same one
-//
-//				let dist = r1.distance - r2.distance
-//				let x = (r2.x! - r1.x!) / dist
-//				let y = (r2.y! - r1.y!) / dist
-//				let z = (r2.z! - r1.z!) / dist
-//				let d = sqrt(pow(r1.distance, 2) - pow(r2.distance, 2) - pow(r1.x!, 2) + pow(r2.x!, 2) - pow(r1.y!, 2) + pow(r2.y!, 2) - pow(r1.z!, 2) + pow(r2.z!, 2))
-//
-//				// Add to matrix A and vector B
-//				a.append(SIMD3<Float>(x, y, z))
-//				b.append(0.5 * d)
-//			}
-//		}
-		
-		
-		// Convert A and B to matrices
-//		var aMat = float3x3(a[0], a[1], a[2])
-//		var bVec = SIMD3<Float>(b[0], b[1], b[2])
-//
-//		for i in 3..<a.count {
-//			aMat += float3x3(a[i-2], a[i-1], a[i])
-//			bVec += SIMD3<Float>(b[i], b[i-1], b[i-2])
-//		}
-		
-
-
-		// Calculate pseudo-inverse of A
-//		guard let aInv = aMat.pseudoInverse() else { return nil }
-		
-		// Calculate position of emitter
-//		let x = (aInv * bMat)[0, 0]
-//		let y = (aInv * bMat)[1, 0]
-//		let z = (aInv * bMat)[2, 0]
-//		let pos = aInv * bVec
-		
-		
-//		return [0,0,0]
-//		return [x, y, z]
 	}
 	
 	/// Reduce the area where the Emitter could be by
